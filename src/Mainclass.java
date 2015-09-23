b91e62fe85b7c11d86e2309366c405d80d006b2d
yuvalgls <yuval.glasman.hit@gmail.com>
2015-09-23 16:15:59 +0300

    #1 test
    fuirts

---
+++ src/Mainclass.java
@@ -0,0 +1,17 @@
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(sum(1,1));
+		System.out.println(sum(3,3));
+		System.out.println(sum(3,6));
+		System.out.println(sum(5,5));
+
+	}
+	
+	public static int sum(int a, int b){
+		return a+b;
+	}
+
+}
